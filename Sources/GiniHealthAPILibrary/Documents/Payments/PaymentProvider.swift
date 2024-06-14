f5a7cebca4a9f240c0c88551aa75f04ca7e68641
Team Mobile Schorsch <team-mobile@gini.net>
2024-06-14 20:46:56 +0000

    Release version 4.1.0

--- Sources/GiniHealthAPILibrary/Documents/Payments/PaymentProvider.swift
+++ Sources/GiniHealthAPILibrary/Documents/Payments/PaymentProvider.swift
@@ -14,12 +14,15 @@ public struct PaymentProvider: Codable {
     public var name: String
     public var appSchemeIOS: String
     public var colors: ProviderColors
-    var minAppVersion: MinAppVersions?
+    public var minAppVersion: MinAppVersions?
     public var iconData: Data
     public var appStoreUrlIOS: String?
     public var universalLinkIOS: String
+    public var index: Int?
+    public var gpcSupportedPlatforms: [PlatformSupported]
+    public var openWithSupportedPlatforms: [PlatformSupported]
 
-    public init(id: String, name: String, appSchemeIOS: String, minAppVersion: MinAppVersions?, colors: ProviderColors, iconData: Data, appStoreUrlIOS: String?, universalLinkIOS: String) {
+    public init(id: String, name: String, appSchemeIOS: String, minAppVersion: MinAppVersions?, colors: ProviderColors, iconData: Data, appStoreUrlIOS: String?, universalLinkIOS: String, index: Int?, gpcSupportedPlatforms: [PlatformSupported], openWithSupportedPlatforms: [PlatformSupported]) {
         self.id = id
         self.name = name
         self.appSchemeIOS = appSchemeIOS
@@ -28,6 +31,15 @@ public struct PaymentProvider: Codable {
         self.iconData = iconData
         self.appStoreUrlIOS = appStoreUrlIOS
         self.universalLinkIOS = universalLinkIOS
+        self.index = index
+        self.gpcSupportedPlatforms = gpcSupportedPlatforms
+        self.openWithSupportedPlatforms = openWithSupportedPlatforms
     }
 }
 public typealias PaymentProviders = [PaymentProvider]
+
+extension PaymentProvider: Equatable {
+    public static func == (lhs: PaymentProvider, rhs: PaymentProvider) -> Bool {
+        lhs.id == rhs.id
+    }
+}
